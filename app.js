444193c0a40773e9163680acd066d304bb1628ef
Kanishka Thakur <kanishkathakur710@gmail.com>
2025-08-12 09:04:57 +0530

    Add backend package.json

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
+        {/* Login Page as the default route */}
         <Route path="/" element={<LoginPage />} />
+        
+        {/* Dashboard Page */}
         <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* Optional: Handle 404 page */}
+        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
